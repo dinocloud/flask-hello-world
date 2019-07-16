1b23c57e1482e38125a43b4d02701d2edafa860c
Franco Zulian <francozulian@dinocloudconsulting.com>
2019-07-16 12:21:45 -0300

    application.py change

--- application.py
+++ application.py
@@ -4,9 +4,9 @@ import os
 application = Flask(__name__)
 
 
-@application.route('/')
+@application.route('/')l
 def hello():
-    return 'Im in staging. i hope go to production'
+    return 'Im a bandito'
 
 if __name__ == '__main__':
     application.run(host=os.getenv('HOST', '0.0.0.0'), port=os.getenv('PORT', '8888'))
